bfa1b70c32f0dcf33b68362388a625b6fb45b1ae
조윤정 <dbswjd1413@naver.com>
2024-08-22 23:57:00 +0900

    [Feat]: 그룹원 추가 Form 구현
    - /addGroupMember로 router 설정
    - InputField와 SelectField 구현
    - AddGroupMemberPage 구현
    - .gitkeep 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
-
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Outlet } from "react-router-dom";
 import axios from "axios";
 
 const queryClient = new QueryClient();
@@ -8,17 +8,22 @@ const queryClient = new QueryClient();
 function App() {
   useEffect(() => {
     const getPosts = async () => {
-      const res = await axios.get("/posts");
-
-      console.log(res.data);
+      try {
+        const res = await axios.get("/posts");
+        console.log(res.data);
+      } catch (error) {
+        console.error("Error fetching posts:", error);
+      }
     };
 
     getPosts();
-  });
+  }, []);
 
   return (
     <QueryClientProvider client={queryClient}>
-      <div className="text-3xl font-bold">App</div>
+      <div className="min-h-screen bg-gray-100">
+        <Outlet />
+      </div>
     </QueryClientProvider>
   );
 }
--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,11 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
-
 import App from "@/App";
+import AddGroupMemberPage from "@/pages/AddGroupMemberPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    children: [
+      {
+        path: "addGroupMember",
+        element: <AddGroupMemberPage />,
+      },
+    ],
   },
 ]);
 
