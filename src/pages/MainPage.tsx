d675c1970aa5b36e31c69e8f352be69494ed7547
gyeongmann <hkc8857@naver.com>
2024-08-23 20:12:49 +0900

    [Design] #9 - 버튼 디자인 수정

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -24,20 +24,40 @@ function MainPage() {
 
         <div className="grid grid-cols-2 gap-x-6 gap-y-4 justify-items-center">
           <div className="bg-white w-[150px] h-[150px] shadow-md rounded-[20px] flex flex-col justify-between p-3">
-            <span className="text-[16pt] font-bold">가디언 평가</span>
-            <IoShieldCheckmark className="w-[44px] h-[44px] self-end" />
+            <div>
+              <span className="text-[16pt] font-bold">가디언 평가</span>
+              <p className="text-[#888888] text-[10pt] mt-1">
+                평가와 관련된 내용을 확인할 수 있습니다.
+              </p>
+            </div>
+            <IoShieldCheckmark className="w-[44px] h-[44px] text-[#167CFA] self-end" />
           </div>
           <div className="bg-white w-[150px] h-[150px] shadow-md rounded-[20px] flex flex-col justify-between p-3">
-            <span className="text-[16pt] font-bold">문제 은행</span>
-            <IoSchool className="w-[44px] h-[44px] self-end" />
+            <div>
+              <span className="text-[16pt] font-bold">문제 은행</span>
+              <p className="text-[#888888] text-[10pt] mt-1">
+                다양한 문제를 모아놓은 공간입니다.
+              </p>
+            </div>
+            <IoSchool className="w-[44px] h-[44px] text-[#167CFA] self-end" />
           </div>
           <div className="bg-white w-[150px] h-[150px] shadow-md rounded-[20px] flex flex-col justify-between p-3">
-            <span className="text-[16pt] font-bold">그룹원 목록</span>
-            <IoPeople className="w-[44px] h-[44px] self-end" />
+            <div>
+              <span className="text-[16pt] font-bold">그룹원 목록</span>
+              <p className="text-[#888888] text-[10pt] mt-1">
+                현재 그룹원의 리스트를 확인하세요.
+              </p>
+            </div>
+            <IoPeople className="w-[44px] h-[44px] text-[#167CFA] self-end" />
           </div>
           <div className="bg-white w-[150px] h-[150px] shadow-md rounded-[20px] flex flex-col justify-between p-3">
-            <span className="text-[16pt] font-bold">프로필 편집</span>
-            <IoPencil className="w-[44px] h-[44px] self-end" />
+            <div>
+              <span className="text-[16pt] font-bold">프로필 편집</span>
+              <p className="text-[#888888] text-[10pt] mt-1">
+                프로필 정보를 수정할 수 있습니다.
+              </p>
+            </div>
+            <IoPencil className="w-[44px] h-[44px] text-[#167CFA] self-end" />
           </div>
         </div>
       </div>
