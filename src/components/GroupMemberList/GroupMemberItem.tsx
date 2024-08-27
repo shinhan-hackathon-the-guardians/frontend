d880eb02371833e728e56dbf259d6a40f22c76c0
yunjeong <dbswjd1413@naver.com>
2024-08-27 09:44:35 +0900

    [Feat]: list의 얼굴 icon 추가

--- src/components/GroupMemberList/GroupMemberItem.tsx
+++ src/components/GroupMemberList/GroupMemberItem.tsx
@@ -3,17 +3,38 @@ import GroupMemberOptions from "./GroupMemberOptions";
 import { Member } from "@/types/Member";
 import { FcBookmark } from "react-icons/fc";
 
+// 개별적으로 이미지 임포트
+import fatherImage from "@/assets/images/groupList/아버지.svg";
+import motherImage from "@/assets/images/groupList/어머니.svg";
+import sonImage from "@/assets/images/groupList/자녀.svg";
+import grandfatherImage from "@/assets/images/groupList/할아버지.svg";
+import grandmotherImage from "@/assets/images/groupList/할머니.svg";
+import siblingImage from "@/assets/images/groupList/형제.svg";
+import etcImage from "@/assets/images/groupList/기타.svg"; // 기타
+
 interface Props {
   member: Member;
   onPinToggle: (isPinned: boolean) => void;
 }
 
+const relationshipImageMap: Record<string, string> = {
+  아버지: fatherImage,
+  어머니: motherImage,
+  자녀: sonImage,
+  할아버지: grandfatherImage,
+  할머니: grandmotherImage,
+  형제: siblingImage,
+  기타: etcImage,
+};
+
 const GroupMemberItem: React.FC<Props> = ({ member, onPinToggle }) => {
+  const imageSrc = relationshipImageMap[member.relationship] || etcImage;
+
   return (
     <div className="relative bg-white rounded-lg shadow-md p-4 flex items-center">
       <div className="flex-shrink-0 mr-4">
         <img
-          src={`https://via.placeholder.com/48`}
+          src={imageSrc}
           alt={member.name}
           className="w-12 h-12 rounded-full"
         />
