44b85aae49750ac41332ce176dda91097c5afa3e
yunjeong <dbswjd1413@naver.com>
2024-08-28 17:33:00 +0900

    [Design]: 로딩 디자인 수정 및 모달 디자인 수정
    - 로딩 디자인 수정
    - 로딩.css 추가
    - WarningModal의 글자 색 변경 및 배치 수정

--- src/components/Question/WarningModal.tsx
+++ src/components/Question/WarningModal.tsx
@@ -21,28 +21,54 @@ const WarningModal: React.FC<WarningModalProps> = ({ isOpen, onClose }) => {
       className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50"
       onClick={handleOverlayClick}
     >
-      <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-[340px]">
-        <h2 className="text-xl font-bold mb-4">시험 안내</h2>
-        <p className="mb-4 text-sm">
-          시험은 총 20개의 OX 문항으로 구성되며, 18개 이상 맞히면 패스합니다.
-          제한 시간 내에 답을 선택해야 하며, 시간 초과 시 자동으로 오답
-          처리됩니다. 제한시간 후 해설이 제공됩니다. 이전 문제로 돌아갈 수
-          없습니다. 신중히 체크해주시기 바랍니다. 시험은 언제든지 재응시할 수
-          있습니다.
-        </p>
-        <div className="flex justify-center space-x-2">
-          <button
-            onClick={onClose}
-            className="px-4 py-2 bg-gray-200 text-gray-800 rounded hover:bg-gray-300"
-          >
-            아니오
-          </button>
-          <button
-            onClick={goToGuardianExam}
-            className="px-4 py-2 bg-Button text-white rounded hover:bg-blue-600"
-          >
-            예
-          </button>
+      <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+        <div className="p-4 flex flex-col justify-center text-center items-center">
+          <h3 className="text-lg font-bold mb-2">시험 안내</h3>
+          <p className="text-sm mb-4">
+            시험은{" "}
+            <span className="text-red-600 font-semibold">
+              총 20개의 OX 문항
+            </span>
+            으로 구성되며,
+            <br />
+            <span className="text-red-600 font-semibold">18개 이상</span> 맞히면
+            패스합니다.
+          </p>
+          <p className="text-sm mb-4">
+            <span className="text-red-600 font-semibold">제한 시간</span> 내에
+            답을 선택해야 하며,
+            <br />
+            시간 초과 시{" "}
+            <span className="text-red-600 font-semibold">
+              자동으로 오답 처리
+            </span>
+            됩니다.
+            <br />
+            제한시간 후 해설이 제공됩니다.
+          </p>
+          <p className="text-sm mb-4">
+            이전 문제로 돌아갈 수 없습니다.
+            <br />
+            신중히 체크해주시기 바랍니다.
+          </p>
+          <p className="text-sm mb-4">시험은 언제든지 재응시할 수 있습니다.</p>
+          <p className="text-base text-red-600 font-semibold mb-4">
+            응시하시겠습니까?
+          </p>
+          <div className="flex justify-end space-x-2">
+            <button
+              className="w-20 px-4 py-2 bg-gray-200 text-gray-800 rounded hover:bg-gray-300 transition-colors duration-200"
+              onClick={goToGuardianExam}
+            >
+              예
+            </button>
+            <button
+              onClick={onClose}
+              className="w-20 px-4 py-2 bg-Button text-white rounded hover:bg-blue-600 transition-colors duration-200"
+            >
+              아니오
+            </button>
+          </div>
         </div>
       </div>
     </div>
--- src/components/common/Loading.tsx
+++ src/components/common/Loading.tsx
@@ -1,11 +1,22 @@
+import "@/assets/css/Loading.css";
+
 const Loading = () => {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <div className="flex flex-row gap-2">
-        <div className="w-4 h-4 rounded-full bg-blue-700 animate-bounce [animation-delay:.7s]"></div>
-        <div className="w-4 h-4 rounded-full bg-blue-700 animate-bounce [animation-delay:.3s]"></div>
-        <div className="w-4 h-4 rounded-full bg-blue-700 animate-bounce [animation-delay:.7s]"></div>
-      </div>
+    <div id="wifi-loader">
+      <svg className="circle-outer" viewBox="0 0 86 86">
+        <circle className="back" cx="43" cy="43" r="40"></circle>
+        <circle className="front" cx="43" cy="43" r="40"></circle>
+        <circle className="new" cx="43" cy="43" r="40"></circle>
+      </svg>
+      <svg className="circle-middle" viewBox="0 0 60 60">
+        <circle className="back" cx="30" cy="30" r="27"></circle>
+        <circle className="front" cx="30" cy="30" r="27"></circle>
+      </svg>
+      <svg className="circle-inner" viewBox="0 0 34 34">
+        <circle className="back" cx="17" cy="17" r="14"></circle>
+        <circle className="front" cx="17" cy="17" r="14"></circle>
+      </svg>
+      <div className="text" data-text="Loading"></div>
     </div>
   );
 };
