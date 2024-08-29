03bb76a9870e533cb29458929da1ddfe8b2dafef
Hyunuk <dksgusdnr17@naver.com>
2024-08-30 03:14:08 +0900

    [Refactor] #48 Add hover css

--- src/components/Question/AnswerButton.tsx
+++ src/components/Question/AnswerButton.tsx
@@ -9,14 +9,9 @@ interface AnswerButtonProps {
   disabled: boolean;
 }
 
-const AnswerButton: React.FC<AnswerButtonProps> = ({
-  type,
-  answer,
-  onClick,
-  disabled,
-}) => {
+const AnswerButton: React.FC<AnswerButtonProps> = ({ type, answer, onClick, disabled }) => {
   const buttonStyle = cn(
-    "w-24 h-24 rounded-full text-3xl bg-white text-gray-600",
+    "w-36 h-48 rounded-lg text-5xl bg-white text-gray-600 font-bold hover:bg-gray-200",
     {
       "border-2 border-gray-300": type === "default",
       "border-4 border-green": type === "correct",
@@ -26,7 +21,17 @@ const AnswerButton: React.FC<AnswerButtonProps> = ({
 
   return (
     <button onClick={onClick} disabled={disabled} className={buttonStyle}>
-      {answer === "correct" ? "O" : "X"}
+      {answer === "correct" ? (
+        <div>
+          <p>O</p>
+          <span className="text-xl">예</span>
+        </div>
+      ) : (
+        <div>
+          <p>X</p>
+          <span className="text-xl">아니오 </span>
+        </div>
+      )}
     </button>
   );
 };
