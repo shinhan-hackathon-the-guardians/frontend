a5db66c7e157c9de0a83f98d4259f7172c6268c1
조윤정 <dbswjd1413@naver.com>
2024-08-30 07:10:46 +0900

    [Feat]: 시험 관련 api 함수 구현
    - 문제 받아오는 api 함수 추가
    - 시험 결과 전송하는 api 함수 추가
    - 문제은행 페이지에서 시험 보러가기 상시 호라성화
    - 합격 상한선 constant에 추가
    - 결과 모달 70% -> 18개로 변경

--- src/services/questionService.ts
+++ src/services/questionService.ts
@@ -1,17 +1,36 @@
-import axiosInstance from './axiosInstance';
+import axiosInstance from "./axiosInstance";
 import { Question } from "@/types/Question";
-import { financeQuestions as dummyFinanceQuestions } from "@/utils/data";
+// import { financeQuestions as dummyFinanceQuestions } from "@/utils/data";
 
+// 가디언 시험 파트
 const getQuestions = async (): Promise<Question[]> => {
-    return dummyFinanceQuestions;
-    try {
-        const response = await axiosInstance.get('/question');
-        return response.data;
-    } catch (error) {
-        console.error('Failed to fetch question bank', error);
-        throw error;
-    }
+  try {
+    const response = await axiosInstance.get("/question");
+    return response.data;
+  } catch (error) {
+    console.error("Failed to get question", error);
+    throw error;
+  }
 };
 
+const postQuestionComplete = async (isPassed: boolean): Promise<void> => {
+  try {
+    await axiosInstance.post<Question[]>("/question/practice", { isPassed });
+  } catch (error) {
+    console.error("Failed to post question result:", error);
+    throw error;
+  }
+};
 
-export { getQuestions };
+// 문제은행 파트
+const getPractice = async (): Promise<Question[]> => {
+  try {
+    const response = await axiosInstance.get("/question");
+    return response.data;
+  } catch (error) {
+    console.error("Failed to get practice", error);
+    throw error;
+  }
+};
+
+export { getQuestions, postQuestionComplete, getPractice };
