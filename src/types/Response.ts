786147e8733e84a1146c04eb9f0704a905786482
Hyunuk <dksgusdnr17@naver.com>
2024-08-31 00:59:40 +0900

    [Feat] #54 Add Chat interface

--- src/types/Response.ts
+++ src/types/Response.ts
@@ -6,3 +6,14 @@ export interface FamilyResponse {
   approval_request: number;
   users: Member[];
 }
+
+export interface ChatBotHistoryResponse {
+  userId: string;
+  message: string;
+  from_user: boolean;
+  timestamp: string;
+}
+
+export interface ChatResponse {
+  message: string;
+}
