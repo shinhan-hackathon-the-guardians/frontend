744294ca581923f9e2b7286e861a2a205bfdeff8
조윤정 <dbswjd1413@naver.com>
2024-08-31 14:22:57 +0900

    [Feat]: 알림 그룹원 초대 리스트로 변환 및 ui 수정

--- src/services/notificationService.ts
+++ src/services/notificationService.ts
@@ -44,6 +44,7 @@ export const notificationService = {
   getUnansweredNotifications: async () => {
     try {
       const response = await axiosInstance.get("/notification/unanswered");
+      console.log(response.data);
       return response.data;
     } catch (error) {
       console.error("🚨 [NotificationService] | Error in getUnansweredNotifications:", error);
