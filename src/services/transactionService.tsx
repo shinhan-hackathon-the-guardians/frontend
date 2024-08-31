3711b19601ec6526a9348b2db3e838d7e7998c1b
Hyunuk <dksgusdnr17@naver.com>
2024-08-31 10:45:27 +0900

    [Feat] #72 add transactio service

---
+++ src/services/transactionService.tsx
@@ -0,0 +1,69 @@
+import axiosInstance from "./axiosInstance";
+
+export const transactionService = {
+  // 입금
+  deposit: async (account_number: string, transaction_balance: number): Promise<any> => {
+    try {
+      const response = await axiosInstance.post(`/transactions/deposit`, {
+        account_number: account_number,
+        transaction_balance: transaction_balance,
+      });
+      return response;
+    } catch (error) {
+      console.error("Failed to deposit request.", error);
+      throw error;
+    }
+  },
+
+  withdrawal: async (account_number: string, transaction_balance: number): Promise<any> => {
+    console.log(transaction_balance);
+    try {
+      const response = await axiosInstance.post(`/transactions/withdrawal`, {
+        account_number: account_number,
+        transaction_balance: transaction_balance,
+      });
+      return response;
+    } catch (error) {
+      console.error("Failed to withdrawal request.", error);
+      throw error;
+    }
+  },
+
+  transfer: async (
+    withdrawal_account_number: string,
+    deposit_account_number: string,
+    transaction_balance: number
+  ): Promise<any> => {
+    console.log(transaction_balance);
+    try {
+      const response = await axiosInstance.post(`/transactions/transfer`, {
+        withdrawal_account_number: withdrawal_account_number,
+        deposit_account_number: deposit_account_number,
+        transaction_balance: transaction_balance,
+      });
+      return response;
+    } catch (error) {
+      console.error("Failed to transfer request.", error);
+      throw error;
+    }
+  },
+
+  payment: async (
+    account_number: string,
+    business_name: string,
+    transaction_balance: number
+  ): Promise<any> => {
+    console.log(transaction_balance);
+    try {
+      const response = await axiosInstance.post(`/transactions/payment`, {
+        account_number: account_number,
+        business_name: business_name,
+        transaction_balance: transaction_balance,
+      });
+      return response;
+    } catch (error) {
+      console.error("Failed to payment request.", error);
+      throw error;
+    }
+  },
+};
